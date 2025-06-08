f6f459ca773a3acef4df79a561e4c33442962a44
aaparicio87 <alaparicio87@gmail.com>
2025-06-08 16:19:24 +0200

    Quit tamagui

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
-import { TamaguiProvider, View } from '@tamagui/core'
-import { config } from './tamagui.config';
 
 export default function App() {
   return (
-    <TamaguiProvider config={config}>
-      <View width={200} height={200} backgroundColor="$black" borderColor="$white" borderWidth="$size.md" />
-    </TamaguiProvider>
+    <></>
   );
 }
 
