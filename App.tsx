3bca973edd5f7e9cd6c3526aba87ae6fe8de4834
aaparicio87 <alaparicio87@gmail.com>
2025-06-08 13:52:41 +0200

    Firts commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider, View } from '@tamagui/core'
+import { config } from './tamagui.config';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider config={config}>
+      <View width={200} height={200} backgroundColor="$black" />
+    </TamaguiProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
