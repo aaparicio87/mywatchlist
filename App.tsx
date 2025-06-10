2bb4390021030659065c2cda6d31e9c9fbfdd61b
aaparicio87 <alaparicio87@gmail.com>
2025-06-10 18:26:15 +0200

    Install gesture handler and bottom sheet

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { KeyboardProvider } from "react-native-keyboard-controller";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { AuthProvider } from '@/contexts/AuthContext/AuthContext';
 import { GlobalThemeProvider } from '@/contexts/ThemeContext/ThemeContext';
 import { MainNavigation } from '@/navigation/MainNavigation/MainNavigation';
-
 
 
 export default function App() {
@@ -14,12 +15,16 @@ export default function App() {
       <KeyboardProvider>
         <AuthProvider>
           <NavigationContainer>
-            <MainNavigation />
+            <GestureHandlerRootView>
+              <BottomSheetModalProvider>
+                <MainNavigation />
+              </BottomSheetModalProvider>
+            </GestureHandlerRootView>
           </NavigationContainer>
         </AuthProvider>
       </KeyboardProvider>
     </GlobalThemeProvider>
-  );
+  )
 }
 
 
